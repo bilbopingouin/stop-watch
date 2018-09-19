5a3370c4504e86148dd2c497d7b6c4cad71e5b17
bilbopingouin <bilbopingouin@gmail.com>
2018-09-19 23:07:00 +0200

    Some code cleanup, improve some functionalities, as well as some graphical features

--- src/stop-watch.c
+++ src/stop-watch.c
@@ -1,21 +1,32 @@
-/* https://www.linuxquestions.org/questions/programming-9/timer-stopwatch-program-in-c-and-ncurses-4175500907/ */
+/* 
+ *  This code was derived from an example found on the following forum:
+ *  https://www.linuxquestions.org/questions/programming-9/timer-stopwatch-program-in-c-and-ncurses-4175500907/ 
+ *  by the user: metashima
+ */
+
+//-- Includes -----------------------
 
 #include <stdio.h>
 #include <time.h>
 
 #include <ncurses.h>
 
-//char number_image [] = {1,2,3};
-//char *number_image [] = {"1","2","3"};
-/* not working
-   char **number_image [] = { 
-   {"1","2","3"},
-   {"4","5","6"},
-   };*/
+//-- Defines ------------------------
 
 #define HISTORY_X   70
 #define HISTORY_Y   1
 #define HISTORY_MAX 10
+
+#define CLOCK_X 10
+#define CLOCK_Y 10
+
+//-- Typedef ------------------------
+typedef enum {
+  WATCH_PAUSED,
+  WATCH_RUN
+} watch_status_typedef;
+
+//-- Private variables --------------
 
 char *numbers_image [] = 
 {
@@ -88,9 +99,18 @@ char *numbers_image [] =
 
 struct timespec history_time[HISTORY_MAX];
 
-/*inline*/ void subtract_time (struct timespec * __restrict later, struct timespec * __restrict former)
+//-- Private functions --------------
+
+/***
+  * Get the current time and calculate the time difference with the 'former' time. 
+  * Input:  former
+  * Output: later
+  */
+
+void subtract_time (struct timespec * __restrict later, struct timespec * __restrict former)
 {
   clock_gettime(CLOCK_MONOTONIC, later);
+
   later->tv_sec -= former->tv_sec;
   if (later->tv_nsec < former->tv_nsec)
   {
@@ -103,7 +123,9 @@ struct timespec history_time[HISTORY_MAX];
   }
 }
 
-
+/***
+  * Print one digit for the clock
+  */
 
 void input_nb(int n, int x, int y)
 {
@@ -113,15 +135,11 @@ void input_nb(int n, int x, int y)
   {
     mvprintw (y+i,x,numbers_image[n*5+i]);
   }
-  /*
-     mvprintw(y+0,x," #### ");
-     mvprintw(y+1,x,"##  ##");     
-     mvprintw(y+2,x,"##  ##");     
-     mvprintw(y+3,x,"##  ##");     
-     mvprintw(y+4,x," #### ");     
-   */
 }
 
+/***
+  * Print history
+  */
 void input_history(int x, int y)
 {
   int i;
@@ -136,6 +154,16 @@ void input_history(int x, int y)
   }
 }
 
+void set_prompt(void)
+{
+  mvprintw (0,0,"");
+  refresh();
+}
+
+/***
+  * Update the history table as well as
+  * refreshing the screen values
+  */
 void refresh_history(struct timespec t)
 {
   int i;
@@ -147,7 +175,29 @@ void refresh_history(struct timespec t)
   history_time[0] = t;
   //printf("\t%d",history_time[0].tv_sec);
   input_history(HISTORY_X,HISTORY_Y);
+  set_prompt();
+  refresh();
 }
+
+/***
+  * Print the clock
+  */
+void input_clock(int x,int y, struct timespec now)
+{
+  input_nb((now.tv_sec / 36000)%10      ,x+0 ,y);
+  input_nb((now.tv_sec / 3600)%10       ,x+7,y);
+  input_nb(10                           ,x+13,y);
+  input_nb((((now.tv_sec/60)%60)/10)%10 ,x+16,y);
+  input_nb((now.tv_sec / 60)%10         ,x+23,y);
+  input_nb(10                           ,x+29,y);
+  input_nb(((now.tv_sec%60)/ 10)%10     ,x+32,y);
+  input_nb((now.tv_sec )%10             ,x+39,y);
+
+  set_prompt();
+  refresh();
+}
+
+//-- Main function ------------------
 
 int main (int argc, char *argv[])
 {
@@ -156,89 +206,90 @@ int main (int argc, char *argv[])
   struct timespec start;
   struct timespec now;
 
+  /* Start the timer */
   clock_gettime(CLOCK_MONOTONIC, &start);
-  if (2 == argc)
-  {
-    sscanf (argv[1], "%u", &countdown);
-    start.tv_sec += countdown * 60;
-  }
-  else if (1 != argc)
-  {
-    printf ("Usage: %s <minute_countdown>\n", argv[0]);
-    return 1;
-  }
 
+  /* Initialise ncurses */
   initscr();
   cbreak();
   noecho();
   nodelay (stdscr, TRUE);
 
+  /* Initialise some structures */
   struct timespec delay;
   delay.tv_sec = 0;
   delay.tv_nsec = 1000000;
 
-
+  /* Watch presentation */
   mvprintw(0,1, "  ##  START-STOP WATCH  ##  ");
-  mvprintw(40,1,"  <ESC>/q: leave  | <SPACE>: start/pause | R: reset to 0");
+  mvprintw(40,1,"  <ESC>/q: leave  | <SPACE>: start/pause | r: reset to 0");
 
   input_history(HISTORY_X,HISTORY_Y);
 
-  int paused = 0;
+  /* Initialise some values */
+  watch_status_typedef state = WATCH_RUN;
   struct timespec prev = start;
+
+  /* Loop program */
   for (;;)
   {
-    if (0 == paused)
+    if (state == WATCH_RUN)
     {
+      /* Calculate the current time since start */
       subtract_time (&now, &start);
+
+      /* Update clock on each second */
       if (now.tv_sec != prev.tv_sec)
       {
-        //mvprintw (1, 2, "%+04d:%+03d", now.tv_sec / 60 , now.tv_sec % 60);
-        //input_nb(0,10,10);
-
-        input_nb((now.tv_sec / 36000)%10      ,10,10);
-        input_nb((now.tv_sec / 3600)%10       ,17,10);
-        input_nb(10                           ,23,10);
-        input_nb((((now.tv_sec/60)%60)/10)%10 ,26,10);
-        input_nb((now.tv_sec / 60)%10         ,33,10);
-        input_nb(10                           ,39,10);
-        input_nb(((now.tv_sec%60)/ 10)%10     ,42,10);
-        input_nb((now.tv_sec )%10             ,49,10);
-
-        refresh();
+        input_clock(CLOCK_X,CLOCK_Y,now);
         prev = now;
       }
     }
     else
     {
+      /* Update the start value */
       subtract_time (&start, &now);
     }
 
+    /* Interaction */
     if (ERR != (ch = getch()))
     {
+      /* Quit the application */
       if (27 == ch || 'q' == ch || 'Q' == ch)
       {
         break;
       }
+      /* Reset the values */
       else if ('r' == ch || 'R' == ch)
       {
-        refresh_history(now);
+        if (state == WATCH_RUN)
+          refresh_history(now);
+
         clock_gettime(CLOCK_MONOTONIC, &start);
+
+        if (state == WATCH_PAUSED)
+        {
+          now.tv_sec = 0;
+          now.tv_nsec = 0;
+          input_clock(CLOCK_X,CLOCK_Y,now);
+        }
       }
-      else
+      else if (' ' == ch)
       {
-        if (0 == paused)
+        if (state == WATCH_RUN)
         {
           refresh_history(now);
-          attron (A_STANDOUT);
-          paused = 1;
+          //attron (A_STANDOUT);
+          state = WATCH_PAUSED;
+          mvprintw (20, 20, "=== PAUSED ===");
+          set_prompt();
         }
         else
         {
-          attroff (A_STANDOUT);
-          paused = 0;
+          //attroff (A_STANDOUT);
+          state = WATCH_RUN;
+          mvprintw (20, 20, "              ");
         }
-        //mvprintw (1, 2, "%+04d:%+03d", now.tv_sec / 60 , now.tv_sec % 60);
-        //refresh_history(now);
         refresh();
       }
     }
